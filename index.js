b0cceff26020f01b420052e82dcfd305dd557f28
Gideon Daniel <gideondaniel@Gideons-MBP.powerhub>
2025-05-25 20:06:24 +0100

    Add 5 days weather prediction with openweather forcast api

--- index.js
+++ index.js
@@ -12,22 +12,60 @@ app.use(express.urlencoded({extended: true}));
 const apiKey = "3c8079c615399d398df5262fc50405d7";
 
 app.get("/", (req,res)=>{
-    res.render("index.ejs", {content: ""});
+    res.render("index.ejs", {content: "", weather: "", forecast: ""});
 })
 
 app.post("/", async(req, res)=>{
     const searchInput = req.body.searchInput;
     try{
-        const result = await axios.get("https://api.openweathermap.org/geo/1.0/direct", {
+        const geoResponseresult = await axios.get("https://api.openweathermap.org/geo/1.0/direct", {   //ALLOWS YOU GET LON & LAT OF A LOCATION BY CITY NAME, ZIPCODE, FULL ADDRESS ETC...
             params:{
-                q: searchInput,
-                limit: 1,
-                appid: apiKey,
+                q: searchInput,  // clients searched location 
+                limit: 1,  // limit means number of same name location to be shown on the api call
+                appid: apiKey, // appid holding your api key for authorization
 
             }
 
         });
-        res.render("index.ejs", {content: result.data[0]});
+        if(!geoResponseresult.data.length){
+            return res.status(404).send(" OOpss Location Not Found..."); // if api not searched properly or location doesnt exist send error 404
+        }
+
+        const {lat, lon} = geoResponseresult.data[0]; // got the lon & lat of the location from the geo api & assigned it a var to use its data to fetch location tempeature
+
+        
+            const weatherResult = await axios.get("https://api.openweathermap.org/data/2.5/weather", { // The weather api is used to get the current weather information (like temperature, weather description, humidity, etc.) for a specific location.
+                params: {
+                    lat, //passed in the longitutude 
+                    lon, //passed in the latitude
+                    units: "metric", // units i wanted in kent
+                    appid: apiKey, // my api key was required as usual for aauthorisation
+
+                }
+            });
+
+         
+            const forecastResult = await axios.get("https://api.openweathermap.org/data/2.5/forecast", {
+                params: {
+                    lat,
+                    lon,
+                    units: "metric",
+                    appid: apiKey
+                }
+            });
+           
+            res.render("index.ejs",
+            {   content: geoResponseresult.data[0], // rendered all geo datas to my home page returning an array []
+               weather: weatherResult.data, // rendered all weather datas to my home page returning an object 
+               forecast: forecastResult.data
+           });
+
+
+      
+
+        
+
+       
     }catch(error){
         console.log(error.response?.data || error.message);
         res.status(401).send("something went wrong");
